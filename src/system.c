ff24a0338dd3c4c44820c544151599cee72a1040
SDK 3400 <sdk3400@ubuntu.(none)>
2015-02-23 11:31:12 -0300

    Implemented System functions. Needs clean up after testing.

--- src/system.c
+++ src/system.c
@@ -101,7 +101,7 @@ mrb_audio_s__beep(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_system_s_reboot(mrb_state *mrb, mrb_value self)
 {
- return mrb_fixnum_value(reboot(RB_AUTOBOOT));
+  return mrb_fixnum_value(reboot(RB_AUTOBOOT));
 }
 
 void
