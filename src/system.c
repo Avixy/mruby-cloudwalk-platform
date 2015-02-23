1a2970fba1d8a6b19fffbba9d9aa27636533f966
SDK 3400 <sdk3400@ubuntu.(none)>
2015-02-23 11:31:12 -0300

    Implemented System functions. Needs clean up after testing.

--- src/system.c
+++ src/system.c
@@ -7,19 +7,33 @@
 #include "mruby/array.h"
 #include "mruby/string.h"
 #include "mruby/hash.h"
+#include <sys/reboot.h>
+
+#include "core.h"
 
 static mrb_value
 mrb_system_s__serial(mrb_state *mrb, mrb_value self)
 {
   char serial[128];
+  uint32_t serial_number;
 
   memset(&serial, 0, sizeof(serial));
+  serial_number = avxGetSerialNumber();
 
-  /*TODO Implement*/
+  sprintf(serial, "%d", serial_number);
 
   return mrb_str_new_cstr(mrb, serial);
 }
 
+/**
+# Set screen backlight (turn on automatically if there has actions like key-pressing, card-swiping or card-inserting).
+    #  0 = Turn off backlight.
+    #  1 = (D200): Keep backlight on for 30 seconds ( auto-shut-down after 30 seconds).
+    #  1 = (Vx510): On.
+    #  2 = (D200): Always on.
+    # -1 = (Avixy 3400): Turn off automatically after xx seconds.
+    #  n = (Evo/Telium 2): Percentage until 100.
+*/
 static mrb_value
 mrb_system_s__set_backlight(mrb_state *mrb, mrb_value self)
 {
@@ -27,19 +41,46 @@ mrb_system_s__set_backlight(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "i", &mode);
 
+  avxSetBacklight(mode, mode);
+
   /*TODO Implement*/
 
   return mrb_fixnum_value(mode);
 }
 
+/**
+ * # Read the battery level.
+# 0 Battery voltage low and battery icon blinks. Suggested that do not process transaction, print and wireless communication etc. at this moment. You should recharge the battery immediately to avoid shut down and lost data.
+# 1 Battery icon displays 1 grid
+# 2 Battery icon displays 2 grids
+# 3 Battery icon displays 3 grids
+# 4 Battery icon displays 4 grids
+# 5 Powered by external power supply and the battery in charging. Battery icon displays form empty to full cycle. The battery status indicator on the bottom of terminal is displaying red
+# 6 Powered by external power supply and the battery charging 6 finished. Battery icon displays full grids. The battery status indicator on the bottom of terminal is displaying green.
+ */
+
 static mrb_value
 mrb_system_s__battery(mrb_state *mrb, mrb_value self)
 {
   char battery[128];
+  struct avx_charger batInfo;
+  int capacity = 0;
 
   memset(&battery, 0, sizeof(battery));
 
-  /*TODO Implement*/
+  memset(&batInfo, 0, sizeof(struct avx_charger));
+
+  if ( avxGetBatteryInfo(&batInfo) < 0 )
+  {
+	  //Report error.
+  } else {
+	   capacity = (batInfo.charge / batInfo.capacity) * 5;
+	  if (batInfo.power_supply_present){
+		  capacity = (capacity == 4 ? 6 : 5);
+	  }
+  }
+
+  sprintf(battery, "%d", capacity);
 
   return mrb_str_new_cstr(mrb, battery);
 }
@@ -51,21 +92,15 @@ mrb_audio_s__beep(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "ii", &tone, &milliseconds);
 
-  /*TODO Implement*/
-  /*OsBeep(tone, milliseconds);*/
+  avxBuzzer(tone, milliseconds, 1);
 
-  return mrb_nil_value();
+  return mrb_fixnum_value(tone);
 }
 
 static mrb_value
 mrb_system_s_reboot(mrb_state *mrb, mrb_value self)
 {
-  mrb_int ret=0;
-
-  /*TODO Implement*/
-  /*ret = OsReboot();*/
-
-  return mrb_fixnum_value(ret);
+  return mrb_fixnum_value(OsReboot());
 }
 
 static mrb_value
