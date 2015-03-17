7b24e0e05e539b3c7cafcd900f744d6fe294cfed
Raul Xavier <raul.xavier@avixy.com>
2015-03-17 15:49:31 -0300

    Implemented basic GPRS support. Fixed the gprs and network dependency and added the avixy device defines

--- src/gprs.c
+++ src/gprs.c
@@ -1,8 +1,11 @@
 
-#include "mruby.h"
-#include <stddef.h>
-#include <string.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h>
+
+#include "mruby.h"
 #include "mruby/array.h"
 #include "mruby/class.h"
 #include "mruby/data.h"
@@ -16,14 +19,54 @@
   #include "mruby/error.h"
 #endif
 
+#ifdef AVIXY_DEVICE
+#include "network/network.h"
+#include "network/modem.h"
+#include "gprs/gprs_interface.h"
+#endif
+
+
+/*TODO: Combinar com o Thiago métodos para:
+ * Set SIM Pin. Existem Operadoras com Simcards protegidos por PIN. Observar que isso deve ser feito antes do start.
+ * Get ID Do simcard.
+ */
+
+
+static int simSlotSelected = 0;
+static char *prvSimPIN="";
+static char prvIdDoSimCard[32];
+
+static char strGPRSApn[32];
+static char strGPRSUser[16];
+static char strGPRSPasswd[16];
+
+#ifdef AVIXY_DEVICE
+int fillAPN(int operadora, char *nomeOperadora, struct avxmodem_access_point *apn, char *nomePadraoDaOperadora, char *simID)
+{
+	int indOperadora = OP_INDEFINIDA;
+
+	strncpy(apn->apn, strGPRSApn, sizeof(apn->apn));
+	strncpy(apn->login, strGPRSUser, sizeof(apn->login));
+	strncpy(apn->password, strGPRSPasswd, sizeof(apn->password));
+
+	strcpy(nomePadraoDaOperadora, nomeOperadora);
+
+	return(indOperadora);
+}
+#endif
+
 /*Start the hardware*/
 static mrb_value
 mrb_gprs_start(mrb_state *mrb, mrb_value klass)
 {
   mrb_int ret=0;
 
-  /*TODO Implement*/
-  /*ret = OsWlLock();*/
+#ifdef AVIXY_DEVICE
+  avxnmInterfacePriority(AVXNM_NETIF_PRIORITIES_MODEM_ETHERNET_WIFI);
+  gprsComSetSimSlot(simSlotSelected);
+  gprsComSetModoOperacaoDesejado(GPRS_MODO_CONECTADO);
+  ret = gprsComInit(prvSimPIN, prvIdDoSimCard, NULL, fillAPN);
+#endif
 
   return mrb_fixnum_value(ret);
 }
@@ -35,11 +78,17 @@ mrb_gprs_power(mrb_state *mrb, mrb_value klass)
   mrb_int on;
   mrb_get_args(mrb, "i", &on);
 
-  /*TODO Implement*/
-  /*OsWlSwitchPower(on);*/
+#ifdef AVIXY_DEVICE
+  if (on){
+	  gprsComSetModoOperacaoDesejado(GPRS_MODO_CONECTADO);
+  }else{
+	  gprsComSetModoOperacaoDesejado(GPRS_MODO_DESLIGADO);
+  }
+#endif
 
   return mrb_true_value();
 }
+
 
 /*Start GPRS dial*/
 /*should be unblocking if timeout 0*/
@@ -59,8 +108,13 @@ mrb_gprs_connect(mrb_state *mrb, mrb_value klass)
   password = mrb_cv_get(mrb, klass, mrb_intern_lit(mrb, "@password"));
   sPass    = mrb_str_to_cstr(mrb, password);
 
-  /*TODO Implement*/
-  /*ret = OsWlLogin(sAPN, sUser, sPass, 0xff, timeout, keep_alive, NULL);*/
+  strncpy(strGPRSApn, sAPN, sizeof(strGPRSApn));
+  strncpy(strGPRSUser, sUser, sizeof(strGPRSUser));
+  strncpy(strGPRSPasswd, sPass, sizeof(strGPRSPasswd));
+
+#ifdef AVIXY_DEVICE  
+  ret = gprsComSetModoOperacaoDesejado(GPRS_MODO_CONECTADO);
+#endif
 
   return mrb_fixnum_value(ret);
 }
@@ -73,9 +127,35 @@ static mrb_value
 mrb_gprs_connected_m(mrb_state *mrb, mrb_value klass)
 {
   mrb_int ret=0;
+  int state;
 
-  /*TODO Implement*/
-  /*ret = OsWlCheck();*/
+#ifdef AVIXY_DEVICE
+  state = gprsComGetCurrentState();
+
+  switch (state)
+	{
+  	  case G_PPP_CONNECTED:
+  		ret = 0;
+  		break;
+	case G_INITIALIZING:
+	case G_READ_SIM_ID:
+	case G_CONNECTING:
+	case G_SEARCHING_GPRS:
+	case G_PPP_CONNECTING:
+	case G_CONNECTED:
+		ret = 1;
+		break;
+	case G_REGISTRATION_REFUSED:
+	case G_PPP_DISCONNECTING:
+	case G_SHUTTING_DOWN:
+	case G_SHUTDOWN:
+		ret = -state;
+		break;
+	default:
+		ret=-99;
+		break;
+	}
+#endif
 
   return mrb_fixnum_value(ret);
 }
@@ -85,8 +165,9 @@ mrb_gprs_disconnect(mrb_state *mrb, mrb_value klass)
 {
   mrb_int ret=0;
 
-  /*TODO Implement*/
-  /*OsWlLogout();*/
+#ifdef AVIXY_DEVICE
+  gprsComSetModoOperacaoDesejado(GPRS_MODO_REGISTRADO);
+#endif
 
   return mrb_true_value();
 }
