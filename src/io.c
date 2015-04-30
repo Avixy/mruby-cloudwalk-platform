3031579939fb53a8701500d23a5deaea47039b70
Raul Xavier <raul.xavier@avixy.com>
2015-04-30 11:29:37 -0300

    keyboard first implementation.

--- src/io.c
+++ src/io.c
@@ -7,6 +7,15 @@
 #include "mruby/array.h"
 #include "mruby/string.h"
 #include "mruby/hash.h"
+#include "core.h"
+#include "gfx.h"
+
+enum input_mode
+{
+	INPUT_NUMBERS = 36,
+  INPUT_LETTERS = 20,
+  INPUT_SECRET  = 28
+};
 
 static mrb_value
 mrb_platform_io_s__getc(mrb_state *mrb, mrb_value self)
@@ -16,14 +25,55 @@ mrb_platform_io_s__getc(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "o", &timeout);
 
-  /*If timeout is nil the function should be blocking
-   *if mrb_fixnum_p(timeout)
-   *  key = GetKey(mrb_fixnum(timeout));
-   *else
-   *  key = GetKey(mrb_fixnum(timeout));
-   */
+  printf("i will get a char with timeout = %d", timeout);
+
+  key = kbdWaitKey(timeout);
 
   return mrb_fixnum_value(key);
+}
+
+static mrb_value mrb_platform_io_s__gets(mrb_state *mrb, mrb_value self)
+{
+	int ret = 666;
+	char sValue[128], mask[128];
+	mrb_int min, max, mode, x, y;
+
+	memset(sValue, '\0', sizeof(sValue));
+	memset(mask, '\0', sizeof(sValue));
+
+	mrb_get_args(mrb, "iiiii", &min, &max, &mode, &y, &x);
+	printf("-------------> min = %d, max = %d, mode = %d, y = %d, x = %d\n",
+			min, max, mode, y, x);
+
+	inputConfig(INPUT_CFG_DEFAULT, 0);
+	inputConfig(INPUT_CFG_TIMEOUT, (void *) 60000);
+	inputConfig(INPUT_CFG_DRAW_BORDER, 0);
+
+	if (mode == INPUT_NUMBERS)
+	{
+		memset(mask, '9', max);
+
+	}
+	else // INPUT_LETTERS / default
+	{
+		memset(mask, 'x', max);
+	}
+
+	lcdSetCursorLC(y, x);
+
+
+	if (mode == INPUT_SECRET)
+	{
+		ret = inputGetFormated(va("!*[%02d]%s", min, mask), sValue);
+	}
+	else
+	{
+		ret = inputGetFormated(va("![%02d]%s", min, mask), sValue);
+	}
+	printf("ret = %d, sValue = %s\n", ret, sValue);
+	//lcdPrint(y, x, sValue);
+
+	return mrb_fixnum_value(0);
 }
 
 void
@@ -31,8 +81,12 @@ mrb_io_init(mrb_state* mrb)
 {
   struct RClass *io;
 
-  io = mrb_class_get(mrb, "IO");
+  io = mrb_define_class(mrb, "IO", mrb->object_class);
 
   mrb_define_class_method(mrb , io , "_getc" , mrb_platform_io_s__getc , MRB_ARGS_REQ(1));
+
+  mrb_define_class_method(mrb , io , "_gets" , mrb_platform_io_s__gets , MRB_ARGS_REQ(5));
+
+  inputConfig(INPUT_CFG_SET_ESCAPE, (void *)'\b');
 }
 
