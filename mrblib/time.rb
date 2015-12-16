30b6531672ff3d51d5f00a2d43a702a7b97feea0
Eduardo Marques <eduardo.marques@avixy.com>
2015-12-16 18:29:09 -0200

    Fixing the set datetime to include the timezone

--- mrblib/time.rb
+++ mrblib/time.rb
@@ -1,6 +1,13 @@
 class Time
-  def hwclock
-    Platform::System.hwclock(self.year, self.month, self.day, self.hour, self.min, self.sec)
+  def hwclock(timezone = nil)
+    if timezone
+      if timezone.include? "+"
+        timezone = timezone.sub("+", "-")
+      elsif timezone.include? "-"
+        timezone = timezone.sub("-", "+")
+      end
+    end
+    Platform::System.hwclock(self.year, self.month, self.day, self.hour, self.min, self.sec, timezone)
   end
 end
 
